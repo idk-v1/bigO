e7363f401cfa38edf03b53cd33bf14b1a59c2089
idk-v1 <benhamilton.jpg@gmail.com>
2025-09-24 12:10:26 -0500

    Add project files.

---
+++ bigO.h
@@ -0,0 +1,124 @@
+#pragma once
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <stdarg.h>
+
+#ifdef _WIN32
+#include <Windows.h>
+#endif
+
+void enableVT()
+{
+#ifdef _WIN32
+	HANDLE hOutput = GetStdHandle(STD_OUTPUT_HANDLE);
+	DWORD dwMode;
+	GetConsoleMode(hOutput, &dwMode);
+	dwMode |= ENABLE_PROCESSED_OUTPUT | ENABLE_VIRTUAL_TERMINAL_PROCESSING;
+	SetConsoleMode(hOutput, dwMode);
+#endif
+}
+
+void sleep(unsigned ms)
+{
+#ifdef _WIN32
+	Sleep(ms);
+#else
+	sleep(ms);
+#endif
+}
+
+
+int lineStart;
+int current = 0;
+int N;
+#define LINECOUNT 18
+#define LABELCOUNT LINECOUNT + 1
+
+#define START(n_) N = n_; lineStart = __LINE__ + 1;
+
+int lines[LINECOUNT] = { 0 };
+const char* lineStr[LINECOUNT];
+const char* labels[LABELCOUNT] = { "" };
+
+void lineInc(int line)
+{
+	lines[line]++;
+}
+
+int* varPtrs[100] = { 0 };
+const char* varNames[100] = { 0 };
+int varArrCounts[100] = { 0 };
+int varsCount = 0;
+void trackVar(void** ptr, const char* name, ...)
+{
+	varNames[varsCount] = name;
+
+	va_list args;
+	va_start(args, name);
+	varArrCounts[varsCount] = va_arg(args, int);
+	va_end(args);
+	varPtrs[varsCount] = (varArrCounts[varsCount] ? *ptr : ptr);
+
+	varsCount++;
+}
+#define TRACK_VAR(var, ...) trackVar(&var, #var, __VA_ARGS__, 0)
+
+unsigned sleepTime = 100;
+
+void printLines()
+{
+	printf("\x1B[?25l\x1B[1;1H");
+	printf("n=%d\n", N);
+
+	for (int i = 0; i < varsCount; i++)
+	{
+		if (varArrCounts[i])
+		{
+			printf("%s = {", varNames[i]);
+			for (int ii = 0; ii < varArrCounts[i]; ii++)
+			{
+				printf("%d", varPtrs[i][ii]);
+				if (ii + 1 == varArrCounts[i])
+					printf("}                                 \n");
+				else
+					printf(", ");
+			}
+		}
+		else
+		{
+			printf("%s = %d                                 \n", varNames[i], *varPtrs[i]);
+		}
+	}
+	printf("-------------------------------------------------------------------------\n");
+
+	for (int i = 0; i < LINECOUNT; i++)
+	{
+		if (labels[i])
+			printf("%-14s ", labels[i]);
+		else
+			printf("               ");
+
+		printf("%s%3d %-50s %3d\x1B[0m\n",
+			current == i ? "\x1B[7m" : "",
+			i,
+			lineStr[i] ? lineStr[i] : "------- NOT EXECUTED -------",
+			lines[i]);
+	}
+	if (labels[LINECOUNT])
+		printf("%-14s\n", labels[LINECOUNT]);
+
+	printf("                                                              \n");
+	sleep(sleepTime);
+}
+
+#define L(line) lineStr[__LINE__ - lineStart] = #line; lineInc(__LINE__ - lineStart); current = __LINE__ - lineStart; printLines(); line
+#define LABEL(label) label: labels[__LINE__ - lineStart] = #label ":";
+#define END current = __LINE__ - lineStart; printLines();
+
+#define IF(cond) if (cond) {
+#define IF_GOTO(label) goto label;}
+#define GOTO(label) goto label;
+#define IS_NOT !=
+#define IS ==
---
+++ main.c
@@ -0,0 +1,93 @@
+#include "bigO.h"
+
+void bubblesort(int x[], int n)
+{
+	int i = 0, ii = 0, temp = 0;
+	TRACK_VAR(i);
+	TRACK_VAR(ii);
+	TRACK_VAR(temp);
+	TRACK_VAR(x, n);
+
+						START(n);
+						L(i = 0;)
+						L(ii = 0;)
+						L(temp = 0;)
+LABEL(OuterLoop)		L(IF (i >= n))
+						L(IF_GOTO(OuterLoopEnd))
+						L(ii = 0;)
+LABEL(InnerLoop)		L(IF (ii >= n - 1 - i))
+						L(IF_GOTO(InnerLoopEnd))
+						L(IF (x[ii] <= x[ii + 1]))
+						L(IF_GOTO(EndIf))
+						L(temp = x[ii + 1];)
+						L(x[ii + 1] = x[ii];)
+						L(x[ii] = temp;)
+LABEL(EndIf)			L(ii = ii + 1;)
+						L(GOTO(InnerLoop))
+LABEL(InnerLoopEnd)		L(i = i + 1;)
+						L(GOTO(OuterLoop))
+LABEL(OuterLoopEnd)		END
+}
+
+void selectionsort(int x[], int n)
+{
+	int smallest = 0, index = 0, temp = 0, i = 0, ii = 0;
+
+	TRACK_VAR(smallest);
+	TRACK_VAR(index);
+	TRACK_VAR(temp);
+	TRACK_VAR(i);
+	TRACK_VAR(ii);
+	TRACK_VAR(x, n);
+
+					START(n)
+					L(i = 0;)
+LABEL(OuterLoop)	L(IF (i >= n - 1))
+					L(IF_GOTO(EndOuterLoop))
+					L(smallest = INT_MAX;)
+					L(ii = i;)
+LABEL(InnerLoop)	L(IF (ii >= n))
+					L(IF_GOTO(EndInnerLoop))
+					L(IF (x[ii] >= smallest))
+					L(IF_GOTO(EndIf))
+					L(smallest = x[ii];)
+					L(index = ii;)
+LABEL(EndIf)		L(ii = ii + 1;)
+					L(GOTO(InnerLoop))
+LABEL(EndInnerLoop)	L(temp = x[i];)
+					L(x[i] = x[index];)
+					L(x[index] = temp;)
+					L(i = i + 1;)
+					L(GOTO(OuterLoop))
+LABEL(EndOuterLoop) END
+
+	//for (i = 0; i < n - 1; i++)
+	//{
+	//	smallest = INT_MAX;
+	//	for (ii = i; ii < n; ii++)
+	//	{
+	//		if (x[ii] < smallest)
+	//		{
+	//			smallest = x[ii];
+	//			index = ii;
+	//		}
+	//	}
+
+	//	temp = x[i];
+	//	x[i] = x[index];
+	//	x[index] = temp;
+	//}
+}
+
+int main(int argc, char** argv)
+{
+	enableVT();
+
+	int x[] = { 57,37,8,7,6,};
+	int n = sizeof(x) / sizeof(int);
+	sleepTime = 200;
+	//bubblesort(x, n);
+	selectionsort(x, n);
+
+	return 0;
+}
